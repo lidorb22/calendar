e663dd3fee60ff7739603db378b1164997c2bff5
לידור בניסתי <321lidor11@gmail.com>
2023-09-19 23:13:19 +0300

    3 try

--- pages/api/storeAPI.js
+++ pages/api/storeAPI.js
@@ -7,15 +7,17 @@ const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL;
 export function useGetStoreById(id) {
   const [store, setStore] = useState(null);
   const { user } = appStore((state) => state);
-  console.log(user);
   useEffect(() => {
     async function api() {
       try {
+        console.log("in try");
         const result = await axios.get(`${baseUrl}/store/`, {
           params: { id },
         });
         setStore(result.data);
-      } catch (error) {}
+      } catch (error) {
+        console.log("in catch");
+      }
     }
     return () => {
       api();
